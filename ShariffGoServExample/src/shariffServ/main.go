b2183a74a02de26369c4d9af316533c6c151354d
TankAT <paul.krenn@edu.fh-joanneum.at>
2017-04-24 00:11:48 +0200

    Prepared everything to include backend components

--- ShariffGoServExample/src/shariffServ/main.go
+++ ShariffGoServExample/src/shariffServ/main.go
@@ -6,6 +6,7 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"fmt"
 )
 
 var tpl *template.Template
@@ -13,6 +14,7 @@ var tpl *template.Template
 type pageData struct {
 	Title string
 	Name string
+	FBShareCount int
 }
 
 func init() {
@@ -30,10 +32,13 @@ func main() {
 	http.HandleFunc("/", idx)
 	http.Handle("/pub/", http.StripPrefix("/pub/", http.FileServer(http.Dir("C:/Users/Paul/git/ShariffGoServ/ShariffGoServExample/src/shariffServ/pub/"))))
 	http.Handle("/favico.ico", http.NotFoundHandler())
+	http.HandleFunc("/shariff/", shariff_backend)
 	http.ListenAndServe(":8080", nil)
 }
 
 func idx(w http.ResponseWriter, req *http.Request) {
+	
+	fmt.Println(req.URL.Path)
 	
 	data := pageData{
 		Title: "Go Title",
@@ -45,6 +50,25 @@ func idx(w http.ResponseWriter, req *http.Request) {
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
+}
+
+func shariff_backend(w http.ResponseWriter, req *http.Request){
+	fb_request :=fmt.Sprintf("%s%s\n", req.Host, req.URL.Path) 
+	
+	counts:=getFBShares(fb_request)
+	
+	fmt.Println(counts)
+}
+
+func getFBShares (url string) int{
+	requesturl := fmt.Sprintf("https://api.facebook.com/method/links.getStats?urls=%s&format=xml", url)
+	log.Println(requesturl)
+	request, _:=http.NewRequest(http.MethodGet, requesturl, nil)
+	client :=&http.Client{}
+	response, _:= client.Do(request)
+	log.Println(response)
+	
+	return 4
 }
 
 func apl(w http.ResponseWriter, req *http.Request) {
